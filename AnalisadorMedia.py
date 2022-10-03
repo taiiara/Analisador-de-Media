edcf19d2dc72fd786b468f39a3e9c1e64a3f9d62
taiiara <taiarachase@gmail.com>
2022-10-03 14:17:37 -0300

    Add files via upload

---
+++ AnalisadorMedia.py
@@ -0,0 +1,16 @@
+# Analisador de média Versão 1.0
+# Por Taiara Regis
+
+nota1 = float(input('Quanto você tirou no primeiro bimestre? '))
+nota2 = float(input('Quanto você tirou no segundo bimestre? '))
+media = (nota1 + nota2) / 2
+
+if media <= 5:
+    print(f'Sua média final é {media:.1f} e você está reprovado(a)!')
+    print(f'Estude mais da próxima vez!')
+elif 5.1 <= media <= 6.9:
+    print(f'Sua média final é {media:.1f} e você está de recuperação!')
+    print('Boa sorte na prova!')
+else:
+    print(f'Sua média final é {media:.1f} e você está aprovado(a)!')
+    print('Parabéns!')
